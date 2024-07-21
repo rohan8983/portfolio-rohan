69b8985c2a1576e77a1cdc07a2f904163c8cf3a6
rohan.chougule <rohanchougule3410@outlook.com>
2024-07-22 03:12:58 +0530

    added new images for projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import InstaVCImg from "../public/assets/projects/PeoplelinkInstaVC.png";
-import CryptoImg from "../public/assets/projects/crypto.jpg";
-import NetflixImg from "../public/assets/projects/netflix.jpg";
-import TwitchImg from "../public/assets/projects/twitch.jpg";
+import hsbcInsuranceUK from "../public/assets/projects/hsbcInsuranceUK.png";
+import LearningMate from "../public/assets/projects/LearningMate.png";
+import feelitlive from "../public/assets/projects/feelitlive.png";
+import zoonga from "../public/assets/projects/zoonga.png";
+import liveSignBermuda from "../public/assets/projects/liveSignBermuda.png";
 
 import { ProjectItem } from "./ProjectItem";
 
@@ -17,26 +19,38 @@ const Projects = () => {
         <div className="grid md:grid-cols-2 gap-8">
           <ProjectItem
             backgroundImg={InstaVCImg}
-            title="InstaVC"
-            tech="React JS"
-            projectUrl="/instavc"
+            title="PeopleLink"
+            tech="ReactJS/NodeJS/MongoDB/JavaScript/HTML/CSS"
+            projectUrl="/peoplelink"
           />
           <ProjectItem
-            backgroundImg={CryptoImg}
-            title="Crypto"
-            tech="React JS"
+            backgroundImg={hsbcInsuranceUK}
+            title="HSBC Insurance UK/HK"
+            tech="ReactJS/Java/AngularJS/Jest/HTML/CSS"
             projectUrl="/crypto"
           />
           <ProjectItem
-            backgroundImg={NetflixImg}
-            title="Netflix"
-            tech="React JS"
+            backgroundImg={LearningMate}
+            title="LearningMate(Client)"
+            tech="ReactJS/NodeJS/.Net/HTML/CSS/Jest"
             projectUrl="/nextflix"
           />
           <ProjectItem
-            backgroundImg={TwitchImg}
-            title="Twitch"
-            tech="React JS"
+            backgroundImg={feelitlive}
+            title="FeelitLIVE"
+            tech="ReactJS/NextJS/NodeJS/MongoDB"
+            projectUrl="/twitch"
+          />
+          <ProjectItem
+            backgroundImg={zoonga}
+            title="Zoonga"
+            tech="ReactJS/NodeJS/TypeScript/HTML/CSS"
+            projectUrl="/twitch"
+          />
+          <ProjectItem
+            backgroundImg={liveSignBermuda}
+            title="LiveSign Bermuda"
+            tech="ReactJS/.Net/HTML/CSS/SQL"
             projectUrl="/twitch"
           />
         </div>
